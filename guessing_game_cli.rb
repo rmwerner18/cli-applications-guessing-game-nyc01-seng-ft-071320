121759020bb257691b9f1ea227684a5c380a12aa
Ryan Werner <rmwerner@umich.edu>
2020-07-12 02:07:49 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -3,7 +3,7 @@ def run_guessing_game
   num = rand(1..6)
   puts "Guess a random number between 1 and 6"
   input = gets.chomp
-  if input.to_i == num
+  if input == num.to_s
     puts "You guessed the correct number!"
   elsif input == "exit"
     puts "Goodbye!"
